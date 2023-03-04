bf1948a8137e1ed0c1705b1bd1dace250b98f131
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-03-04 11:13:25 +0600

    accuracy done

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ data.tools?.forEach(tooll=>{
     <p><span>2.</span>${tooll.features[1]}</p>
     <p><span>3.</span>${tooll.features[2]}</p>
     <br>
-   
+
     <h4> ${tooll.name}</h4>
     <div class="d-flex justify-content-between">
     <p><span><i class="fa-solid fa-calendar-days"></i></span> ${tooll.published_in}</p>
@@ -49,30 +49,57 @@ let url=` https://openapi.programming-hero.com/api/ai/tool/${id}`
 fetch(url)
 .then(res =>res.json())
 .then(data =>showAllData(data.data))
+//.then (data =>console.log(data.data))
 }
+
 const showAllData =idDetails =>{
-console.log(idDetails)
+  console.log(idDetails)
+//console.log(idDetails.pricing[0].price);
 const exampleModal=document.getElementById('exampleModal');
 const modalDiv =document.getElementById('exampleModalLabel')
 modalDiv.innerHTML=`
-<div class='d-flex' g-3>
-<div class="card style="width: 10rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">${idDetails}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+
+<div class="row p-2">
+<div class="card col-sm-6  bg-warning">
+<p class='p-3'>${idDetails.description}</p>
+
+<div class='card-group gap-2 d-flex'>
+ <div class="card p-1 text-info">${idDetails.pricing[0] ? idDetails.pricing[0].price : 'No-pricing'}<br><span>${idDetails.pricing[0].plan}<span>  </div>
+ <div class="card p-1 text-warning">${idDetails.pricing[1] ? idDetails.pricing[1].price : 'no price' }<br><span>${idDetails.pricing[1].plan}<span> </div>
+ <div class="card p-1 text-danger">${idDetails.pricing[2] ? idDetails.pricing[1].price: 'no price'  }<br><span>${idDetails.pricing[2].plan}<span> </div>
+ </div>
+<br>
+ <div class="d-flex">
+ <div>
+ <h4>Fetures</h4>
+ <ul>
+ <li>${idDetails.features[1].feature_name}</li>
+ <li>${idDetails.features[2].feature_name}</li>
+ <li>${idDetails.features[3].feature_name}</li>
+ </ul>
+ </div>
+ <div>
+ <p>Integration</p>
+ <ul>
+ <li>${idDetails.integrations[0]}</li>
+ <li>${idDetails.integrations[1]}</li>
+ <li>${idDetails.integrations[2]}</li>
+ </ul>
+ </div>
+ </div>
+
 </div>
-<div class="card style="width: 10rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+ 
+
+<div class="card col-sm-6" style="width:20rem">
+  <img class="card-img-top" src="${idDetails.image_link[0]}">
+  <div class="align-top" >
+  <button class="btn btn-primary p-1" style="margin-top:-100px;">${ idDetails?.accuracy? idDetails.accuracy.score : 'no'} accuracy</button></div><br>
+  <h3>${idDetails.input_output_examples[0]? idDetails.input_output_examples[0].input : 'Can you give any example'}</h3> 
+  <h6>${idDetails.input_output_examples[0]?  idDetails.input_output_examples[0].output: 'No! Not Yet! Take a break!!!'}</h6>
 </div>
 </div>
+
 `
 
 
